1cdf89cbb6989cb6295e804c47e3f465d70573ee
waffleflopper-laptop <46244948+waffleflopper@users.noreply.github.com>
2019-01-01 01:47:12 -1000

    background gradient generator
    changes background and generates linear-gradient code for a 'to right' config.

---
+++ background-generator/script.js
@@ -0,0 +1,28 @@
+/* 
+
+get cssOutput area to print css code for user
+get the colors from the input
+
+add a listener that changes the backgrounds linear-gradient on input
+for each color
+
+output css code to cssOutput (create a textnode and shove it in that bitch)
+
+*/
+
+var css = document.querySelector("#cssOutput");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+
+var body = document.querySelector("#gradient");
+
+function changeBG() {
+    body.style.background = "linear-gradient(to right, "
+     + color1.value + ", " + color2.value + ")";
+
+     css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", changeBG);
+
+color2.addEventListener("input", changeBG);
