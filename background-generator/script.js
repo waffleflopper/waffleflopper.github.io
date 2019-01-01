4fbcc9a10d51b1c25da3fbb08017a248b8a692e2
waffleflopper-laptop <46244948+waffleflopper@users.noreply.github.com>
2019-01-01 01:52:47 -1000

    small change to start with text displayed

--- background-generator/script.js
+++ background-generator/script.js
@@ -16,11 +16,13 @@ var color2 = document.querySelector(".color2");
 
 var body = document.querySelector("#gradient");
 
+css.textContent = "background: linear-gradient(to right, " + color1.value + ", " + color2.value + ");";
+
 function changeBG() {
     body.style.background = "linear-gradient(to right, "
      + color1.value + ", " + color2.value + ")";
 
-     css.textContent = body.style.background + ";";
+     css.textContent = "background: " + body.style.background + ";";
 }
 
 color1.addEventListener("input", changeBG);
